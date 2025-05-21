141bae97d4af6041200afd158c03ebe5959dd3a5
Lenard-Mate <57682558+Lenard-Mate@users.noreply.github.com>
2025-05-21 18:49:34 +0300

    First commit: Create a project.

---
+++ src/main/java/com/ss/global_main/GlobalMainApplication.java
@@ -0,0 +1,13 @@
+package com.ss.global_main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalMainApplication.class, args);
+	}
+
+}
